90a8c284e407956402a5fc1cc62144257baf5a87
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-02 15:08:58 +0530

    Add files via upload

---
+++ swape2number.java
@@ -0,0 +1,32 @@
+
+
+
+            class swape2number
+
+             {
+
+              public static void main(String[]args)
+
+                 {
+
+                int a=3;
+                   
+              int b=5;
+
+              int c;
+                
+               c=a;
+
+           a=b;
+
+             b=c;
+
+
+          System.out.println("a="+a);
+
+                System.out.println("b="+b);
+
+
+                }
+
+                 }          
