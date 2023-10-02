33752ae195048aed2023cd986374f3961487cdee
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-02 15:57:46 +0530

    Add files via upload

---
+++ alphabet.java
@@ -0,0 +1,26 @@
+
+
+
+
+              class sample  
+
+                {
+
+                 public static void main(String[]args)
+
+                 {
+
+                char value ='A';
+
+                 if(value>='a'&& value<='z' || value>='A'&& value<='Z')
+
+                       System.out.println("alphabet");
+
+
+                    else
+
+                           System.out.println("not alphabet");
+
+
+                             }
+                }
