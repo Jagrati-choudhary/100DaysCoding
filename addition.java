645e2a609cb1a62749fbeae5bcf4d9a51389ebfa
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-02 15:05:56 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,27 @@
+
+
+
+           class addition 
+            {
+
+          public static void main(String[]args)
+
+              {
+
+                int a=2;
+                 
+                int b=4;
+
+                    int c=5;
+
+                  int d=5;
+
+                      int e=6;
+
+                  int add=a+b+c+d+e;
+             System.out.println("addition="+add);
+
+
+            }
+
+   }
