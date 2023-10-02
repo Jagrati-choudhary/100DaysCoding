b910a6c654e80af97e6113a245253d881514c273
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-02 12:07:12 +0530

    Add files via upload

---
+++ division1.java
@@ -0,0 +1,26 @@
+              
+
+
+              class division1 
+                
+             {
+
+             public static void main(String[]args)
+
+
+              {
+
+                   int num=3;
+
+
+                  if(num%3==0||num%7==0)
+
+                         System.out.println("num is divisible");
+
+                  else
+
+                           System.out.println("num is not divisible");
+
+                       }
+
+                  }
