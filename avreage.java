d520c7e132e55a8a92f6477c86cad33541945334
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-02 15:06:21 +0530

    Add files via upload

---
+++ avreage.java
@@ -0,0 +1,26 @@
+
+
+         class avreage 
+            {
+
+          public static void main(String[]args)
+
+           {
+
+            int a=3;
+
+             int b=4;
+
+            int c=5;
+
+             int d=5;
+
+             int e=6;
+
+            int  ave=(a+b+c+d+e)/5;
+
+             System.out.println(ave);
+
+                 }
+
+               }
