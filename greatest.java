6648ebd93ec658b18e713773fa83b0b2207c234a
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-02 15:55:48 +0530

    Add files via upload

---
+++ greatest.java
@@ -0,0 +1,47 @@
+
+
+
+
+
+               class greatest
+
+             {
+
+                  public static void main(String[]args)
+
+                {
+
+                int a=3;
+
+             int b=2;
+
+                  int c=1;
+             
+               if(a>b&&a>c)
+
+                     System.out.println(a+":is largest number");
+
+            else   if(b>a&&b>c)
+
+
+                     System.out.println(b+":is a largest number");
+
+                 else 
+
+                      System.out.println(c+":is a largest number");
+
+               if(a<b&&a<c)
+
+                       System.out.println(a+":is a samllest number");
+
+          else   if(b<a&&b<c)
+
+                          System.out.println(b+":is a samllest number");
+
+             else
+
+                            System.out.println(c+":is a samllest number");
+
+
+             }
+          }
