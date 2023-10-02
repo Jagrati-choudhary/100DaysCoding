6fcde7e43375656d722330e56672189c45f3615a
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-02 12:08:05 +0530

    Add files via upload

---
+++ multiplyof5.java
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+                 class multiplyof5
+
+                  {
+
+              public static void main(String[]args)
+
+                 {
+
+                  int a=5;
+
+                 if(a%5==0)
+
+                     System.out.println(a+"is a multiply of 5");
+
+               else 
+
+                      System.out.println(a+"is a not multiply of 5");
+
+
+                        }
+
+                  }
