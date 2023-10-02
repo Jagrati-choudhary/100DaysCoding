627447eca6204bc9ab72cc429c60ce451ebe8fc3
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-02 12:07:39 +0530

    Add files via upload

---
+++ easy.java
@@ -0,0 +1,17 @@
+
+
+
+               class easy
+
+              {
+
+                   public static void main(String[]args)
+
+              {
+
+               System.out.println("Easy Hai Code\nEasy Hai Code \nEasy Hai Code \nEasy Hai Code \n Easy Hai Code");
+
+                    
+           }
+
+         }
