5294066c449f29dce61cf28fb9bd400d3294baa1
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-02 12:06:00 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,24 @@
+
+
+       class positive
+
+           {
+
+        public static void main(String[]args)
+
+        {
+
+        int num=34;
+
+        if(num>0)
+
+              System.out.println("num is positive");
+
+     else 
+
+              System.out.println("num is nagative");
+
+
+              }
+
+     }
