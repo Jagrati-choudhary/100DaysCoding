1cf5636c856cbd224aa272486996060c4715ce98
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-02 15:57:02 +0530

    Add files via upload

---
+++ between.java
@@ -0,0 +1,31 @@
+
+
+
+
+                class between
+
+                 {
+
+                  public static void main(String[]args)
+
+                {
+
+                    int num=45;
+
+             if(num>=21 && num<=575)
+
+                  System.out.println("yes");
+
+
+               else 
+
+                        System.out.println("no");
+
+
+                    }
+
+            }
+
+
+ 
+                            
