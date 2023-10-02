9359d1117161517276da077c8a7813719559bbd1
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-02 15:55:13 +0530

    Add files via upload

---
+++ voterornon.java
@@ -0,0 +1,27 @@
+
+
+
+              class voterornon
+
+              {
+
+                public static void main(String[]args)
+
+
+               {
+
+            int age=45;
+
+            if(age>=18)
+
+                   System.out.println("voter");
+
+         else
+
+
+                System.out.println("non voter");
+
+
+                 }
+
+         }
