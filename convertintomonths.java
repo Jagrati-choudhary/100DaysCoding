beff9b87b454d185407c2bf4a4653908835eb00e
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-02 15:58:07 +0530

    Add files via upload

---
+++ convertintomonths.java
@@ -0,0 +1,20 @@
+
+
+              class convertintomonths
+
+                  {
+
+                  public static void main(String[]args)
+
+                  {
+
+                     int age=18;
+
+                  int totalmonths=age*12;
+         
+
+                 System.out.println("months="+totalmonths);
+
+                     }
+
+                 }
