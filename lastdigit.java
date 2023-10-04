d3bf8c7cd90c675d7a989f5f71cb1ad7ab647fc5
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-04 18:08:40 +0530

    Add files via upload

---
+++ lastdigit.java
@@ -0,0 +1,24 @@
+
+
+
+
+                           import java.util.Scanner;
+                           class lastdigit
+
+                             {
+
+                                public static void main(String[]args)
+
+                                {
+
+                                 System.out.println("please enter the number");
+
+                               Scanner sc=new Scanner(System.in);
+
+                                  int num =sc.nextInt();
+
+                                 System.out.println("the last digit number is="+(num%10));
+
+                                  }
+
+                              }
