f4a57c666ddd76227f4ecef70f33c3e6eddf7c63
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-04 18:09:51 +0530

    Add files via upload

---
+++ weekday.java
@@ -0,0 +1,43 @@
+
+                            import java.util.Scanner;
+
+                          class weekday
+                             {
+
+                            public static void main(String[]args)
+                             {
+                         
+                         Scanner sc =new Scanner(System.in);
+
+                       int daynum = sc.nextInt();
+
+                         
+                         if(daynum ==0)
+
+                          System.out.println("Sunday");
+                            
+                      else if(daynum ==1)
+
+                          System.out.println("monday");
+                            
+                       else if(daynum ==2)
+
+                          System.out.println("thuseday");
+                            
+                      else if(daynum ==3)
+
+                          System.out.println("Wensday");
+                            
+                      else if(daynum ==4)
+
+                          System.out.println("Friday");
+                            
+                       else if(daynum ==5)
+
+                          System.out.println("Saterday");
+                            
+                       else if(daynum ==6)
+
+                          System.out.println("try agian");
+                          }
+                   }  
