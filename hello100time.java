89d7ee98c292855127e76d48b5a577c64a3ad62b
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-05 19:45:36 +0530

    Add files via upload

---
+++ hello100time.java
@@ -0,0 +1,25 @@
+
+
+                                class hello100time
+
+                                {
+
+                                   public static void main(String[]args)
+
+                                    {
+
+                                  int i =1;
+
+                                  while(i<=100)
+
+                                  {
+
+                                   System.out.println("Hello Java");
+
+                                           i++;
+
+                                   }
+
+                               }
+
+                               }
