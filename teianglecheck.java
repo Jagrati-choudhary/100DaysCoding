4950afce028bbb6833b151c3acc101516d0cc5c4
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-05 18:55:03 +0530

    Add files via upload

---
+++ teianglecheck.java
@@ -0,0 +1,39 @@
+
+
+                                           import java.util.Scanner;
+
+                                            class trianglecheck
+
+                                           {
+
+                                            public static void main(String[]args)
+
+                                       {
+
+                                        Scanner sc = new Scanner(System.in);
+
+                                            System.out.println("enter the angle1");
+
+                                        int angle1 = sc.nextInt();
+
+                                      
+                                            System.out.println("enter the angle2");
+
+                                        int angle2 = sc.nextInt();
+
+                                            System.out.println("enter the angle3");
+
+                                        int angle3 = sc.nextInt();
+
+                                            if((angle1+angle2+angle3)==180)
+
+                                             System.out.println("the triangle is valid");
+
+                                         else
+
+                                         System.out.println("the triangle is not valid");  
+
+                                             }
+
+                                            }
+   
