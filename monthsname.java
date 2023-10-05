9dbfbc516e7e5f91cdb6f2a0bdee556643039ed9
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-05 18:55:30 +0530

    Add files via upload

---
+++ monthsname.java
@@ -0,0 +1,42 @@
+
+
+                            
+                            import java.util.Scanner;
+
+                          class monthsname
+                             {
+
+                            public static void main(String[]args)
+                             {
+                         Scanner sc =new Scanner(System.in);
+                        System.out.println("enter the month number");
+                       int monthnum = sc.nextInt();
+                         if(monthnum ==1)
+                          System.out.println("january");                            
+                      else if(monthnum ==2)
+                          System.out.println("februry");
+                       else if(monthnum ==3)
+                          System.out.println("march");
+                      else if(monthnum ==4)
+                          System.out.println("April");                           
+                      else if(monthnum ==5)
+                          System.out.println("may");                            
+                       else if(monthnum ==6)
+                          System.out.println("june");                          
+                       else if(monthnum ==7)
+                       System.out.println("july");
+                        else if (monthnum ==8)
+                          System.out.println("August");                            
+                      else if(monthnum ==9)
+                          System.out.println("september");
+                       else if(monthnum ==10)
+                          System.out.println("october");
+                      else if(monthnum ==11)
+                          System.out.println("novmber");                           
+                      else if(monthnum ==12)
+                          System.out.println("December");                            
+                       else if(monthnum ==13)
+                          System.out.println("Invalid month number ");                          
+                     
+                          }
+                   }  
