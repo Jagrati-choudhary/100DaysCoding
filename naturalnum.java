276b3005a449e31f55889623c77aef52ca2cc982
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-05 19:46:17 +0530

    Add files via upload

---
+++ naturalnum.java
@@ -0,0 +1,30 @@
+
+
+
+
+                            
+
+
+                                class naturalnum
+
+                                 {
+
+                                   public static void main(String[]args)
+
+                               {
+
+                                   int i=1;
+
+                                  while(i<=10)
+
+                                 {
+
+                                  System.out.println("The natural number is :=" +i);
+
+                                    i++;
+
+                                       }
+
+                                  }
+
+                              }
