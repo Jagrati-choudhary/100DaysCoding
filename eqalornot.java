f470d9066f1dc2bb02096eac900e0753fb66cb7c
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-06 19:47:38 +0530

    Add files via upload

---
+++ eqalornot.java
@@ -0,0 +1,39 @@
+
+
+                            
+
+                   import java.util.Scanner;
+
+                      class equalornot
+
+                         {
+
+                          public static void main(String[]args)
+
+                      {
+
+                        Scanner sc = new Scanner(System.in);
+
+                           System.out.println("enter the first digit num ");
+
+                             int num1 = sc.nextInt();
+
+                              System.out.println("enter the secound digit num ");
+
+                             int num2 = sc.nextInt();
+   
+                             System.out.println("enter the third digit num ");
+
+                             int num3 = sc.nextInt();
+
+                                  if (num1 == num2 && num2 == num3)
+
+                                         System.out.println("number is equal");
+
+                                      else  
+
+                                            System.out.println("number is not equal");
+
+                                    }
+
+                                     }
