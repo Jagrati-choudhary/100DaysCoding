f1aa2352a23dc7cedb10897e679788e9d5bc3074
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-06 19:46:32 +0530

    Add files via upload

---
+++ absolutvalue.java
@@ -0,0 +1,27 @@
+                            import java.util.Scanner;
+
+                            class absolutvalue
+
+                            {
+
+                           public static void main(String[]args)
+
+                           {
+
+                            Scanner sc = new Scanner(System.in);
+
+                             System.out.println(" enter the value");
+
+                             int a =sc.nextInt();
+
+                              if(a>=0)
+
+                                      System.out.println("the absolutvalue is :="+a);
+
+                            else
+
+                                        System.out.println("the absolutvalue is:="+(-a));
+
+                               }
+
+                         }
