aa03daeef2648047cf6c50dc9be0cb318f054ddd
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-06 19:47:08 +0530

    Add files via upload

---
+++ increasingdecreasing.java
@@ -0,0 +1,43 @@
+
+
+
+                       
+                   import java.util.Scanner;
+
+                      class increasingdecreasing
+
+                         {
+
+                          public static void main(String[]args)
+
+                      {
+
+                        Scanner sc = new Scanner(System.in);
+
+                           System.out.println("enter the number ");
+
+                             int number1 = sc.nextInt();
+
+                              System.out.println("enter the  number ");
+
+                             int number2 = sc.nextInt();
+   
+                             System.out.println("enter the  number ");
+
+                             int number3 = sc.nextInt();
+
+                                if(number1 < number2 && number2 < number3)
+
+                                       System.out.println("number is increasing order");
+
+                                    else if (number1 > number2 && number2 > number3)
+
+                                             System.out.println("number is decreasing order");
+
+                                          else
+
+                                                System.out.println("number are not specific order");
+
+                                                }
+
+                                             }
