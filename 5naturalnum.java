fb1a4d86c12bc296e128c5a29bb7e39adc08bc61
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-07 19:20:26 +0530

    Add files via upload

---
+++ 5naturalnum.java
@@ -0,0 +1,27 @@
+
+
+                                           class main
+
+                                 {
+
+                                   public static void main(String[]args)
+
+                               {
+
+                                   int i=1;
+                                   int sum=0;
+
+                                  while(i<=5)
+
+                                 {
+
+                                      sum=sum+i;
+
+                                            i++;
+                                     }
+
+                                  System.out.println("The sum of 5 natural number is :=" +i);
+
+                              
+                                       }
+                               }
