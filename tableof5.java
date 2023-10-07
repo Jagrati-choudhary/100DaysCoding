aca5cd395513e28de01e1e4cd9e6df37d20f865c
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-07 19:20:50 +0530

    Add files via upload

---
+++ tableof5.java
@@ -0,0 +1,26 @@
+
+
+
+                                      class tableof5
+                                   
+                                       {
+
+                                    public static void main(String[]args)
+
+                                   {
+
+                                          int i=1, n=5;
+
+                                          while(i<=10)
+
+                                           {
+
+                                            System.out.println(" Table ="+(n*i));
+
+                                               i++;
+
+                                          }
+
+                                          }
+
+                                     }
