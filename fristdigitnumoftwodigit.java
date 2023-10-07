9be2b9d6073975009fcb1b165c8d1eb79d78dc24
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-07 18:25:45 +0530

    Add files via upload

---
+++ fristdigitnumoftwodigit.java
@@ -0,0 +1,31 @@
+
+
+
+                               
+                               import java.util.Scanner;
+
+                             class fristdigitnumoftwodigit
+
+                               {
+
+                                public static void main(String[]args)
+
+                                  {
+
+                              Scanner sc = new Scanner(System.in);
+
+                                 System.out.println("Enter the two digit number");
+
+                                int num = sc.nextInt();
+
+                                   if(num >=10  && num <=99)
+
+                                       System.out.println("The frist digit is :"+(num/10));
+
+                                     else
+
+                                              System.out.println("number is incorret");
+
+                                                     }
+
+                                                   }
