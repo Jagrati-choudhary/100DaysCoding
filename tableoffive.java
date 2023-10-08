7d8f16228d877f2d486c678ce3e8c617c6e9d903
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-08 15:38:38 +0530

    Add files via upload

---
+++ tableoffive.java
@@ -0,0 +1,25 @@
+
+
+
+
+                                        
+                                             class tableoffive
+
+                                           {
+
+                                           public static void main(String[]args)
+
+                                            {
+
+                                           int i;
+
+                                             for(i=1;i<=10;i++)
+                                              {
+
+                                               System.out.println("5*"+i+"  =  " + (5*i));
+
+                                                     }
+
+                                                  }
+
+                                                    }
