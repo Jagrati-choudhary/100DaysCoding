16c2269f46cba3770f5b43ae3d272e044edbc116
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-08 15:39:14 +0530

    Add files via upload

---
+++ sumofnatruralnum.java
@@ -0,0 +1,27 @@
+
+
+                              class sumofnaturalnum
+
+                                   {
+
+                               public static void main(String[]args)
+                           {
+
+                               int sum=0;
+
+                               for(i=1;i<=5;i++)
+
+                                {
+
+                                   sum = sum+i;
+
+                                   }
+
+                                   System.out.println("sum of natural num =" +sum);
+
+                                   }
+
+                                }
+
+                                
+                               
