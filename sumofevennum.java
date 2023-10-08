5b6020433cf77c8247d6b46751a961021c2c5a8d
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-08 15:39:46 +0530

    Add files via upload

---
+++ sumofevennum.java
@@ -0,0 +1,29 @@
+
+
+                                           class sumofevennum
+
+                                           {
+
+                                           public static void main(String[]args)
+
+                                            {
+
+                                            int sum =0,i;
+
+                                             for(i=1;i<=10;i++)
+
+                                              if(i%2 == 0)
+
+                                              {
+
+                                               System.out.println(i);
+
+                                               sum = sum+i;
+
+                                              }
+
+                                               System.out.println(" sum of even sum ="+sum);
+
+                                                 }
+
+                                                }
