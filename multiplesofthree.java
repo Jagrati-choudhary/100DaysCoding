8ea18e61696d1d4b9471a6bd293c04ce5df01658
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-09 17:53:35 +0530

    Add files via upload

---
+++ multiplesofthree.java
@@ -0,0 +1,26 @@
+
+                                           
+                                                             
+
+                                                             class multiplesofthree
+                                                             {
+
+                                                             public static void main(String[]args)
+
+                                                              {
+
+                                                                                int sum=0;
+
+                                                               for(int i=1;i<=40;i++){
+                                                                if(i%3 == 0){
+                                                               sum = sum + i;
+                                                                   
+
+                                                                   System.out.println(i);  
+                                                            }
+                                                        }
+                                                           System.out.println("sum = " +sum);
+
+                                                               }
+
+                                                            }
