849b80dce76f0d6c712b92209df8714751866d61
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-09 17:52:06 +0530

    Add files via upload

---
+++ sumoffristeventhirteennum.java
@@ -0,0 +1,25 @@
+
+
+                                 
+                                                            
+                                                             class sumoffristeventhirteennum
+
+                                                             {
+
+                                                             public static void main(String[]args)
+
+                                                              {
+
+                                                          int sum=0;
+
+                                                               for(int i= 1; i<=26;i++){
+
+                                                                    sum = sum+i;
+
+                                                                   }
+
+                                                                     System.out.println("sum of ="+sum);
+
+                                                                   }
+
+                                                                   }
