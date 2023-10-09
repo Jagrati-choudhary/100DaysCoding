0564077f9a985cdcf810ab2cf221dec705c6e8f4
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-09 17:52:41 +0530

    Add files via upload

---
+++ naturalnumbers.java
@@ -0,0 +1,27 @@
+
+
+                                                             import java.util.Scanner;
+
+                                                             class naturalnumbers
+
+                                                             {
+
+                                                             public static void main(String[]args)
+
+                                                              {
+
+                                                                Scanner sc = new Scanner(System.in);
+                                                                System.out.println("Enter the number");
+
+                                                                int n = sc.nextInt();
+
+                                                               for(int i=1;i<=n;i++){
+
+                                                                System.out.println("Natural numbers is="+i);
+
+                                                                   }
+
+                                                                 }
+
+                                                                 }
+
