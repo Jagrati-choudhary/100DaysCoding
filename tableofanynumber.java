5110ede6da59013c8bf70db1a3071d09cc5ad217
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-10 17:45:33 +0530

    Add files via upload

---
+++ tableofanynumber.java
@@ -0,0 +1,27 @@
+
+
+                                               import java.util.Scanner;
+                                             
+                                             class tableofanynumber
+
+                                           {
+
+                                           public static void main(String[]args)
+
+                                            {
+                                            Scanner sc = new Scanner(System.in);
+                                            System.out.println("Enetr the numbers");
+                                              int num = sc.nextInt();
+
+                                           int i;
+
+                                             for(i=1;i<=10;i++)
+                                              {
+
+                                               System.out.println("num*"+i+"  =  " + (num*i));
+
+                                                     }
+
+                                                  }
+
+                                             }
