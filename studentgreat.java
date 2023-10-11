189711de22c2d073e0386f4845f8d6fe614873fa
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-11 12:34:26 +0530

    Add files via upload

---
+++ studentgreat.java
@@ -0,0 +1,20 @@
+
+                                                                            import java.util.*;
+                           class studentgreat
+                            {
+
+                              public static void main(String[] args)
+                               {
+                               Scanner sc = new Scanner(System.in);
+                                System.out.println("Enter the number");
+                                 int a = sc.nextInt();
+                                  
+                                   if(a>=75)
+                                     System.out.println("The student great is A");
+                                        else if(a>=50)
+                                          System.out.println("The student great is B");
+                                       else if(a<=30)
+                                       System.out.println("The student great is C");
+
+      }
+}
