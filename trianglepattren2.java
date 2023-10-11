0bee4c02c8b0d0fd9f4f1c7769619fecc1546c45
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-11 12:39:37 +0530

    Add files via upload

---
+++ trianglepattren2.java
@@ -0,0 +1,23 @@
+                        import java.util.*;
+                           class trianglepattren2
+                            {
+
+                              public static void main(String[] args)
+                               {
+                               Scanner sc = new Scanner(System.in);
+                                System.out.println("Enter the row");
+                                 int row = sc.nextInt();
+                                  
+                                   for(int i=1;i<=row;i++)
+                                      {
+                                           for(int j=1; j<=i;j++)
+                                          {
+                                          System.out.print(i+"");
+                                          
+                                          }
+                                          System.out.print("\n");
+
+
+                                                }
+      }
+}
