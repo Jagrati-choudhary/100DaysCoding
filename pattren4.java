e00677e9bb0a416c7abc62996ce1fcde82ac0295
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-11 12:36:29 +0530

    Add files via upload

---
+++ pattren4.java
@@ -0,0 +1,39 @@
+
+
+
+                                          
+
+
+                                          
+
+                              
+                             
+
+                                
+
+
+                           import java.util.*;
+                           class pattren4
+                            {
+
+                              public static void main(String[] args)
+                               {
+                               Scanner sc = new Scanner(System.in);
+                                System.out.println("Enter the row");
+                                 int row = sc.nextInt();
+                                  System.out.println("Enter the col");
+                                 int col = sc.nextInt();
+                                   for(int i=5;i>=row;i--)
+                                      {
+                                           for(int j=5; j>=col;j--)
+                                          {
+                                          System.out.print(j*j);
+                                          }
+                                          System.out.print("\n");
+
+
+                                                }
+                                           }
+                                        
+                                             }
+                                          
