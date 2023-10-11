e8d9ac268b80ccee25b6fe0624f5589ca0b57e7e
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-11 12:34:05 +0530

    Add files via upload

---
+++ distinct.java
@@ -0,0 +1,15 @@
+                         class distinct
+                            {
+                              public static void main(String[]args)
+                                   {
+                             int a=40;
+                              int b=45;
+                             int c=50;
+                            int d=55;
+                             int e=75;
+                             if(a>=75)
+                                System.out.println("The student got distinct");
+                                else
+                                 System.out.println("The student did not get distinct");
+                                 }
+                               }
