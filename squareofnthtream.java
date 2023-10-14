abad5f13038a295d56880a67ee586758ec9c5323
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-14 19:20:44 +0530

    Add files via upload

---
+++ squareofnthtream.java
@@ -0,0 +1,20 @@
+
+                           import java.util.*;
+                           class squareofnthtream
+                            {
+
+                              public static void main(String[] args)
+                               {
+                               Scanner sc = new Scanner(System.in);
+                                System.out.println("Enter the number");
+                              int n=sc.nextInt();
+                                 int i=1;
+                                int square;
+                                do
+                                   {
+                                     square = i*i;
+                                    System.out.println("i  is  =" +square);
+                                    i++;
+                                     }while(i<=n);
+                                     }
+                                      }
