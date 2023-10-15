fe4b161002b25944c8fc6dcd80ac796c21c71ca3
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-15 15:16:05 +0530

    Add files via upload

---
+++ printthe2series.java
@@ -0,0 +1,19 @@
+                        import java.util.*;
+                           class printthe2series
+                            {
+
+                              public static void main(String[] args)
+                               {
+                               Scanner sc = new Scanner(System.in);
+                                System.out.println("Enter the number");
+                              int n=sc.nextInt();
+                               
+                                int series=1;
+                               for(int i=1;i<=n;i++)
+                                   {
+                              series= series * 2;
+                                   System.out.print(series+ " ");
+                                   }
+                                 
+                                  }
+                                 }
