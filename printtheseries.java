54005fae56f89bf6c1c1e2449e7cbf415d903893
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-15 15:16:45 +0530

    Add files via upload

---
+++ printtheseries.java
@@ -0,0 +1,20 @@
+
+                           import java.util.*;
+                           class printtheseries
+                            {
+
+                              public static void main(String[] args)
+                               {
+                               Scanner sc = new Scanner(System.in);
+                                System.out.println("Enter the number");
+                              int n=sc.nextInt();
+                               
+                                int a=0;
+                               for(int i=1;i<=n;i++)
+                                   {
+                              a = a+1;
+                                   System.out.print(a+ " ");
+                                   }
+                                 
+                                  }
+                                 }
