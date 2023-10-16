7cfe9246b94f708177971d18ffc0edf68a143e03
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-10-16 19:26:14 +0530

    Add files via upload

---
+++ digitpower.java
@@ -0,0 +1,35 @@
+                              import java.util.*;
+                                 class digitpower
+                              {
+
+                              public static void main(String[] args)
+                               {
+                               Scanner sc = new Scanner(System.in);
+                                System.out.println("Enter the number");
+                              int n=sc.nextInt();
+                              int power = 1,d,revers =0;
+                               int digit=0;                               
+                                
+                              while(n!=0)
+                              {
+                              d=n%10;
+                              revers = revers*10+d;
+                              n = n/10;
+                              ++digit;
+                                 }
+                                System.out.println("numbers of digit " +digit);
+                                System.out.println("digit raised to the power of " +digit);
+                                   for(int i=1;i<=digit;i++)
+                                        {
+                                    d=revers % 10;
+                                  int res =1;
+                                   for(int j=1;j<=digit;j++)
+                                    {
+                                   res = res*d;
+                                   }
+                                    revers = revers /10;
+                                    System.out.println(res);
+                                    }
+                                       }
+                                          }
+                                 
