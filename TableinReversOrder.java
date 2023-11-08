4c52b9008e794f4521d0d9f08a1545adafc465bf
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-11-08 14:58:52 +0530

    Add files via upload

---
+++ TableinReversOrder.java
@@ -0,0 +1,20 @@
+
+                                               import java.util.Scanner;
+                                             
+                                             class TableinReversOrder
+
+                                           {
+
+                                           public static void main(String[]args)
+
+                                            {
+                                            Scanner sc = new Scanner(System.in);
+                                          
+                                            System.out.println("Enetr the  table numbers");
+                                              int t  = sc.nextInt();
+                                          for(int i=10;i>=1;i--)
+                                         System.out.println(t+" * "+i+" = " +(t*i));
+                                              
+                                                  }
+
+                                             }
