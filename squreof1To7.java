a5cc6549ffd308045a3809d518059f09fa0af123
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-11-08 15:01:25 +0530

    Add files via upload

---
+++ squreof1To7.java
@@ -0,0 +1,20 @@
+  import java.util.*;
+                           class square
+                            {
+
+                              public static void main(String[] args)
+                               {
+                               Scanner sc = new Scanner(System.in);
+                                System.out.println("Enter the number");
+                              int n=sc.nextInt();
+                               
+                                int square;
+                               for(int i=1;i<=n;i++)
+                                   {
+                                  
+                                    square = i*i;
+                                   System.out.println(" square is =" +square);
+                                    }
+                                    }
+                                  }
+                                 
