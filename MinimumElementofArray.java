92e2fbe4c03c3744f086bfbd58aa7f7b72729887
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-11-09 18:02:23 +0530

    Add files via upload

---
+++ MinimumElementofArray.java
@@ -0,0 +1,26 @@
+ import java.util.Scanner;
+
+class MinimumElementofArray{
+	public static void main(String args[]) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number: ");
+		int n = sc.nextInt();
+		int[] a = new int[n];
+
+
+		for(int i=0; i<n; i++) {
+			System.out.print("a["+i+"]=");
+			a[i] = sc.nextInt();
+		}
+		int min = a[0];
+		for(int i=1; i<n; i++) {
+			if(min>a[i]) {
+				min=a[i];
+			}
+		}
+			System.out.print("Minimum element =  "+min);
+
+
+}
+}
