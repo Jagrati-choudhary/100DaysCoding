77f561e57e6d27e0bcbbe927a7e983daa64dcafb
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-11-10 11:29:08 +0530

    Add files via upload

---
+++ teacherpackage2.java
@@ -0,0 +1,18 @@
+package com.code.school; 
+import school.teacher.newjoin.*;
+  class newteacher2
+{
+
+public static void main(String[]args){
+teacherpackage s1=new teacherpackage();
+s1.setName("Jagrati Choudhary");
+s1.setYear(2023);
+s1.setSalary(20000);
+s1.setMobno(0083456767L);
+
+System.out.println(s1.getName());
+System.out.println(s1.getYear());
+System.out.println(s1.getSalary());
+System.out.println(s1.getMobno());
+}
+}
