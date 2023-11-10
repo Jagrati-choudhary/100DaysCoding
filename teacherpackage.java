78a7d79ff636fe57f0b34e3975d69440ebaf4772
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2023-11-10 11:28:47 +0530

    Add files via upload

---
+++ teacherpackage.java
@@ -0,0 +1,39 @@
+package school.teacher.newjoin;
+ public class teacherpackage
+{
+ private String name;
+ private int year;
+ private int salary;
+ private long mobno;
+ public static void main(String[]args){
+}
+
+public void setName(String Name){
+this.name=Name;
+}
+public void setYear(int Year){
+this.year=Year;
+}
+public void setSalary(int Salary){
+this.salary=Salary;
+}
+public void setMobno(long Mobno){
+this.mobno=Mobno;
+}
+
+public String getName(){
+return this.name;
+}
+public int getYear(){
+return this.year;
+}
+public int getSalary(){
+return this.salary;
+}
+public long getMobno(){
+return this.mobno;
+}
+}
+
+
+
